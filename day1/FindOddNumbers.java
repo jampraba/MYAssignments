553310ee5300250d8190ded9b76a13a68e9f400f
jampraba <jamunaaprabakaran@gmail.com>
2024-07-25 17:40:07 +0530

    Add files via upload
    day 1

---
+++ day1/FindOddNumbers.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class FindOddNumbers {
+
+	public static void main(String[] args) {
+		int num = 10;
+	    for(int i=0;i<=num;i++) { 
+	    	if(i%2!=0) {
+	    		System.out.println("Odd numbers is "+i);
+	    	}
+	    }
+			
+		
+	}
+
+}
---
+++ day1/Firefox.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		//dataType variableName= value
+		//variable -local variable, Global variable
+		//local variable- create inside the main method -brown color
+		//global variable- create inside the class level -green color
+		int releaseyear = 1998;
+		float browserVersion = 100.2f;
+		char browserlogo = 'f';
+		String browsername = "Firefox";
+		boolean isVisible = true;
+		System.out.println("The browser name is" + browsername);
+		System.out.println("The browser version is "+ browserVersion);
+		System.out.println("will be launched in the year "+ releaseyear);
+		System.out.println("the browser logo is " + browserlogo);
+	    System.out.println("is browser visible"+ isVisible);
+	    
+			
+		
+		
+				
+	    
+
+	}
+
+}
